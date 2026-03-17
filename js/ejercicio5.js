9da064198329dd91ab73aaae5d593decf1c329f4
castillokfirex <lehernan.07@gmail.com>
2026-03-17 06:12:20 -0500

    feat: :sparkles: ejerecicios 4-8 agregados

---
+++ js/ejercicio5.js
@@ -0,0 +1,9 @@
+let numero = 10;
+let texto = "Hola";
+let booleano = true;
+let sinValor;
+
+console.log("Tipo de numero:", typeof numero);
+console.log("Tipo de texto:", typeof texto);
+console.log("Tipo de booleano:", typeof booleano);
+console.log("Tipo de sinValor:", typeof sinValor);
---
+++ js/ejercicio8.js
@@ -0,0 +1,9 @@
+let numero1 = 10;
+let numero2 = 0;
+
+let resultado1 = numero1 / numero2;
+
+let resultado2 = -numero1 / numero2;
+
+console.log("Resultado positivo:", resultado1);
+console.log("Resultado negativo:", resultado2);
